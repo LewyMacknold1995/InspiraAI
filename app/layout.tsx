358444db9187ae8241a7186ad67fb75c48092a0c
Lewy Macknold <lewymacknold@Lewys-Air.lan>
2024-06-06 17:48:51 +0100

    finished clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Variable } from "lucide-react";
 
 const IBMPlex = IBM_Plex_Sans({ subsets: ["latin"],
   weight: ['400', '500', '600', '700'],
@@ -20,7 +21,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider appearance={{
+      variables: {colorPrimary: '#624cf5'}
+    }}>
       <html lang="en">
         <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
           {children}
