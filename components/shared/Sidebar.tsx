a71d2ac177fcd4db5ffd096438e82631272f9caa
Lewy Macknold <lewymacknold@Lewys-MacBook-Air.local>
2024-06-07 15:30:53 +0100

    feat(Sidebar): added dynamic links to create a list of navigation items

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -1,19 +1,40 @@
-import Link from 'next/link'
-import React from 'react'
-import Image from 'next/image'
+import Link from 'next/link';
+import React from 'react';
+import Image from 'next/image';
+import { SignedIn } from '@clerk/nextjs';
+import { navLinks } from '@/constants';
+import { usePathname } from 'next/navigation';
 
 const Sidebar = () => {
-  return (
-    <aside className="sidebar">
-        <div className="flex size-full flex-col gap-4">
-            <Link href="/" className="sidebar-logo">
-                <Image src="/assets/images/logo-text.svg" alt="logo" width={180} height={28}/>
-            </Link>
+    const pathname = usePathname();
 
-        </div>
+    return (
+        <aside className="sidebar">
+            <div className="flex size-full flex-col gap-4">
+                <Link href="/" className="sidebar-logo">
+                    <Image src="/assets/images/logo-text.svg" alt="logo" width={180} height={28} />
+                </Link>
 
-    </aside>
-  )
-}
+                <nav className="sidebar-nav">
+                    <SignedIn>
+                        <ul className="sidebar-nav_elements">
+                            {navLinks.map((link) => {
+                                const isActive = link.route === pathname;
 
-export default Sidebar
+                                return (
+                                    <li key={link.route} className={`sidebar-nav_element group ${isActive ? 'bg-purple-gradient text-white' : 'text-gray-700'}`}>
+                                        <Link href={link.route}>
+                                            {link.label}
+                                        </Link>
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </SignedIn>
+                </nav>
+            </div>
+        </aside>
+    );
+};
+
+export default Sidebar;
