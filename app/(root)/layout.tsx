4718282ad2ad4f18a4ef4f69ca707e74353553d5
Lewy Macknold <lewymacknold@Lewys-MacBook-Air.local>
2024-06-11 20:28:28 +0100

    feat(mobileNavBar): created new component and imported it

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -5,7 +5,7 @@ const Layout = ({ children }: {children: React.ReactNode}) => {
   return (
     <main className="root">
       <Sidebar />
-      {/* {<MobileNav>} */}
+      <MobileNav />
 
 
         <div className="root-container">
