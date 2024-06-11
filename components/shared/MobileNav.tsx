b0949a78fada3401450ec9b160b5be52de82cf7b
Lewy Macknold <lewymacknold@Lewys-MacBook-Air.local>
2024-06-11 20:58:58 +0100

    added shadcn sheet

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -1,8 +1,50 @@
 import React from 'react'
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+  } from "@/components/ui/sheet"
+  import Link from "next/link"
+  import Image from 'next/image'
+import { SignedIn } from '@clerk/nextjs'
+  
 
 const MobileNav = () => {
   return (
-    <div>MobileNav</div>
+    <header className="header">
+        <Link href="/" className="flex items-center gap-2 md:py-2">
+            <Image 
+                src="/assets/image/logo-text.svg
+                alt="logo"
+                width={180}
+                height={28} />
+        </Link>
+        <nav className="flex gap-2">
+            <SignedIn>
+                <UserButton afterSignOutUrl="/"/>
+                <Sheet>
+  <SheetTrigger>
+    <Image 
+        src="/assets/icons/menu.svg"
+        alt="menu"
+        width={32}
+        height={32}
+        className="cursor-pointer"
+    
+    />
+  </SheetTrigger>
+  <SheetContent className="sheet-content sm:w-64">
+  </SheetContent>
+</Sheet>
+
+            </SignedIn>
+
+        </nav>
+
+    </header>
   )
 }
 
