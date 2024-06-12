b096abe092f72d231eb287b134a566419013e612
Lewy Macknold <lewymacknold@Lewys-MacBook-Air.local>
2024-06-12 20:26:17 +0100

    feat(mobileNavBar): added navar content mapping from the sidebar

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React from 'react';
 import {
     Sheet,
@@ -7,13 +9,16 @@ import {
 import Link from "next/link";
 import Image from 'next/image';
 import { SignedIn, UserButton } from '@clerk/nextjs';
+import { navLinks } from '@/constants';
+import { usePathname } from 'next/navigation';
 
 const MobileNav = () => {
+    const pathname = usePathname(); 
     return (
         <header className="header">
             <Link href="/" className="flex items-center gap-2 md:py-2">
                 <Image 
-                    src="/assets/image/logo-text.svg"
+                    src="/assets/images/logo-text.svg" 
                     alt="logo"
                     width={180}
                     height={28} 
@@ -39,9 +44,23 @@ const MobileNav = () => {
                                 width={152}
                                 height={23}
                             />
+                            <ul className="sidebar-nav_elements">
+                                {navLinks.map((link) => {
+                                    const isActive = link.route === pathname;
+
+                                    return (
+                                        <li key={link.route} className={`sidebar-nav_element group flex items-center gap-2 p-2 rounded-md ${isActive ? 'bg-purple-gradient text-white' : 'text-gray-700 hover:bg-gray-200'}`}>
+                                            <Link href={link.route} className="sidebar-link flex items-center gap-2">
+                                                <Image src={link.icon} alt={link.label} width={24} height={24} className={`${isActive ? 'brightness-200' : 'brightness-100'}`} />
+                                                <span className="sidebar-label">{link.label}</span>
+                                            </Link>
+                                        </li>
+                                    );
+                                })}
+                            </ul>
                         </SheetContent>
                     </Sheet>
-                {/* </SignedIn> */}
+                {/* </SignedIn>  */}
             </nav>
         </header>
     );
