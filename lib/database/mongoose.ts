08d5fda0bbafd23c25faa5669578bfa208dc3102
Lewy Macknold <lewymacknold@Lewys-Air.lan>
2024-06-22 19:07:52 +0100

    everytime we connect to our database, we check if theres a cached connection, if there is, well exit out immediatly if not well make a new connection to mongodb

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -7,3 +7,26 @@ interface MongoosConnection {
     promise: Promise<Mongoose> | null;
 }
 
+let cached: MongoosConnection = (global as any).mongoose
+
+if(!cached) {
+    cached = cached = (global as any).mongoose = { 
+    conn: null, promise: null 
+    }
+} 
+
+export const connectToDatabase = async () => {
+    if(cached.conn) return cached.conn;
+
+    if(!MONGODB_URL) throw new Error('Missing MONGODB_URL is not defined');
+
+    cached.promise = 
+        cached.promise || 
+        mongoose.connect (MONGODB_URL, { 
+            dbName: 'inspiraAi', bufferCommands: false
+        })
+
+    cached.conn = await cached.promise;
+
+    return cached.conn;
+}
